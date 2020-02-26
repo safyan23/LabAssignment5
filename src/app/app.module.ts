4852fb3aa6837f13f78a8cffbe462cba4666191e
Safyan Iqbal <safyan23@Safyans-MacBook-Pro.local>
2020-02-25 21:26:13 -0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './main-header/main-header.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { MainFooterComponent } from './main-footer/main-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    MainContentComponent,
+    MainFooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main-header/main-header.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-header',
+  templateUrl: './main-header.component.html',
+  styleUrls: ['./main-header.component.css']
+})
+export class MainHeaderComponent implements OnInit {
+
+  private firstName: string;
+  private lastName: string;
+
+  constructor() {
+    this.firstName = 'Safyan';
+    this.lastName = 'Iqbal';
+  }
+
+
+  showGreeting() {
+    const banner = document.getElementById('banner');
+    banner.innerHTML = 'Hello ' + this.firstName + ' ' + this.lastName + '.';
+  }
+
+  hideGreeting() {
+    const banner = document.getElementById('banner');
+    banner.innerHTML = 'r1c2';
+  }
+
+  ngOnInit() {
+  }
+
+}
